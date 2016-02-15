59fd1ffde2c24c851e18ebf3b1b498e72a8dcfce
Jidde Koekoek <jiddekoekoek@macbook-pro-van-jidde.local>
2016-02-15 14:06:34 +0100

    Initial Commit

---
+++ Mad LibsTests/Mad_LibsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mad_LibsTests.swift
+//  Mad LibsTests
+//
+//  Created by Jidde Koekoek on 15/02/16.
+//  Copyright © 2016 Jidde Koekoek. All rights reserved.
+//
+
+import XCTest
+@testable import Mad_Libs
+
+class Mad_LibsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
